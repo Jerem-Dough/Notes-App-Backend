08b58cf77f728a9d5225d324e0a949103d2eab47
Jeremy Dougherty <143429873+jerem-dough@users.noreply.github.com>
2024-10-06 00:32:35 -0600

    Update server.js

--- server.js
+++ server.js
@@ -146,11 +146,11 @@ app.get('/', (req, res) => {
   res.send('Welcome to the Notes App API');
 });
 
-// Handle Unknown Routes
+
 app.use((req, res) => {
   res.status(404).json({ message: 'API route not found' });
 });
 
-// Start Server
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
